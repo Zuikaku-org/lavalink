330a5890fcc17c944f3c90e40ae5880a65b52824
Orchit07 <orchitiadi@gmail.com>
2022-04-07 10:49:24 +0700

    chore(config): Remove unused import

--- src/main/kotlin/lavalink/server/config/SpotifyConfig.kt
+++ src/main/kotlin/lavalink/server/config/SpotifyConfig.kt
@@ -1,7 +1,5 @@
 package lavalink.server.config
 
-import xyz.gianlu.librespot.audio.decoders.AudioQuality
-
 class SpotifyConfig {
     var spotifyUsername: String? = null
     var spotifyPassword: String? = null
